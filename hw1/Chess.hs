f31ea231359bcab822cab32c6906e34d66a63426
T. Brandon Ashley <tb@tba.sh>
2016-09-21 12:43:39 -0400

    supress warnings, tabs -> 2 spaces

--- hw1/Chess.hs
+++ hw1/Chess.hs
@@ -13,7 +13,7 @@ type File     = Char         -- column index
                              -- valid files are 'a','b',...,'h'
 type Rank     = Int          -- row index
                              -- valid ranks are 1,2,...8
-type Position = (File,Rank)   
+type Position = (File,Rank)
 
 data Color =
   Black | White
@@ -36,60 +36,60 @@ isLegalFile c
 
 isLegalRank :: Rank -> Bool
 isLegalRank i
-     | i >= 1 && i <= 8 = True 
+     | i >= 1 && i <= 8 = True
      | otherwise = False
 
--- see Rules - Movement for legal movements 
+-- see Rules - Movement for legal movements
 
 isLegalMove :: Color -> Piece -> Position -> Position -> Bool
 -- implement isLegalMove
-isLegalMove _ King (a,b) (c,d) = 
-	if abs((ord a) - (ord c)) <= 1 && abs(b-d) <= 1 
-		&& isLegalPosition  (c,d)
-		then True
-		else False
+isLegalMove _ King (a,b) (c,d) =
+  if abs((ord a) - (ord c)) <= 1 && abs(b-d) <= 1
+    && isLegalPosition  (c,d)
+    then True
+    else False
 
 isLegalMove _ Queen (a,b) (c,d) =
-	if abs((ord a) - (ord c)) ==0 && abs(b - d)>=1 && isLegalPosition(c,d)
-		then True
-		else if abs((ord a)-(ord c)) >=1 && abs(b-d) ==0 && isLegalPosition(c,d)
-			then True
-			else if abs((ord a) - (ord c)) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)			
-				then True
-				else False
+  if abs((ord a) - (ord c)) ==0 && abs(b - d)>=1 && isLegalPosition(c,d)
+    then True
+    else if abs((ord a)-(ord c)) >=1 && abs(b-d) ==0 && isLegalPosition(c,d)
+      then True
+      else if abs((ord a) - (ord c)) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)
+        then True
+        else False
 
 isLegalMove _ Bishop (a,b) (c,d) =
-	if abs((ord a)) - (ord c) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)
-		then True
-		else False
+  if abs((ord a)) - (ord c) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)
+    then True
+    else False
 
 isLegalMove _ Rook (a,b) (c,d) =
-	if abs((ord a)-(ord c)) /=0 && abs(b-d) ==0 && isLegalPosition (c,d)
-		then True
-		else if abs((ord a)-(ord c)) ==0 && abs(b-d)/=0 && isLegalPosition(c,d)
-			then True
-			else False
+  if abs((ord a)-(ord c)) /=0 && abs(b-d) ==0 && isLegalPosition (c,d)
+    then True
+    else if abs((ord a)-(ord c)) ==0 && abs(b-d)/=0 && isLegalPosition(c,d)
+      then True
+      else False
 
 isLegalMove Black Pawn (a,b) (c,d) =
-	if abs((ord a)-(ord c))==0 && b - d == 1 && isLegalPosition(c,d)
-		then True
-		else if abs((ord a)-(ord c))==0 && b - d == 2 && b == 7 && isLegalPosition(c,d)
-			then True
-			else False
+  if abs((ord a)-(ord c))==0 && b - d == 1 && isLegalPosition(c,d)
+    then True
+    else if abs((ord a)-(ord c))==0 && b - d == 2 && b == 7 && isLegalPosition(c,d)
+      then True
+      else False
 
 isLegalMove White Pawn (a,b) (c,d) =
-	if abs((ord a)-(ord c))==0 && d - b == 1 && isLegalPosition(c,d)
-		then True
-		else if abs((ord a)-(ord c))==0 && d - b == 2 && b==2 && isLegalPosition(c,d)
-			then True
-			else False
+  if abs((ord a)-(ord c))==0 && d - b == 1 && isLegalPosition(c,d)
+    then True
+    else if abs((ord a)-(ord c))==0 && d - b == 2 && b==2 && isLegalPosition(c,d)
+      then True
+      else False
 
 isLegalMove _ Knight (a,b) (c,d) =
-	if abs((ord a)-(ord c))==1 && abs(b-d)==2 && isLegalPosition(c,d)
-		then True
-		else if abs((ord a)-(ord c))==2 && abs(b-d) == 1 && isLegalPosition(c,d)
-			then True
-			else False 
+  if abs((ord a)-(ord c))==1 && abs(b-d)==2 && isLegalPosition(c,d)
+    then True
+    else if abs((ord a)-(ord c))==2 && abs(b-d) == 1 && isLegalPosition(c,d)
+      then True
+      else False
 
 isLegalMove _ _ _ _ = False
 
--- hw1/PolygonArea.hs
+++ hw1/PolygonArea.hs
@@ -3,8 +3,8 @@ module PolygonArea where
 computeArea :: [(Double,Double)] -> Double
 computeArea [] = error "KURWA"
 computeArea [a] = error "MACH"
-computeArea (a:xs) = (0.5) * abs(det (last xs) a + det a (head xs) + 
-	computeArea' xs)
+computeArea (a:xs) = (0.5) * abs(det (last xs) a + det a (head xs) +
+  computeArea' xs)
 
 det :: (Double,Double) -> (Double,Double) -> Double
 det (x1,y1) (x2,y2) = (x1*y2) - (x2*y1)
