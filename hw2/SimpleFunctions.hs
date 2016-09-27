5e15f509f52c67603f9f7dfd2a1b131dcae01da8
T. Brandon Ashley <tb@tba.sh>
2016-09-27 15:55:21 -0400

    finish SimpleFunctions a

--- hw2/SimpleFunctions.hs
+++ hw2/SimpleFunctions.hs
@@ -1,21 +1,28 @@
 module SimpleFunctions where
 
+import Data.List
+import System.IO
+
 -- a)
 filterFirst :: (a -> Bool) -> [a] -> [a]
+filterFirst _ []   = []
+filterFirst b (a:as)
+    | b a       = a : filterFirst b as
+    | otherwise = as
 
 -- b)
-filterLast :: (a -> Bool) -> [a] -> [a]
-
--- c)
-split :: [a] -> ([a],[a])
-
--- d)
-interleave :: ([a],[a]) -> [a]
-
--- e)
-merge :: (Ord a) => ([a],[a]) -> [a]
-
--- f)
-mergeSort :: (Ord a) => [a] -> [a]
+--filterLast :: (a -> Bool) -> [a] -> [a]
+--
+---- c)
+--split :: [a] -> ([a],[a])
+--
+---- d)
+--interleave :: ([a],[a]) -> [a]
+--
+---- e)
+--merge :: (Ord a) => ([a],[a]) -> [a]
+--
+---- f)
+--mergeSort :: (Ord a) => [a] -> [a]
 
 
