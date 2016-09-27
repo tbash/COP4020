ea3e4cf9b7fa0c49a03b4ae2d4b4d52c31c6560f
T. Brandon Ashley <tb@tba.sh>
2016-09-27 17:21:10 -0400

    finish SimpleFunctions c

--- hw2/SimpleFunctions.hs
+++ hw2/SimpleFunctions.hs
@@ -15,12 +15,22 @@ filterLast :: (a -> Bool) -> [a] -> [a]
 filterLast _ [] = []
 filterLast b a
     | b (last a) = filterLast b (init a) ++ [last a]
-    | otherwise = init a
+    | otherwise  = init a
 
----- c)
---split :: [a] -> ([a],[a])
---
----- d)
+-- c)
+split :: [a] -> ([a],[a])
+split []        = ([], [])
+split [a]       = ([a], [])
+--split (a:b:abs) = (a:as, b:bs)
+--    where (as, bs) = split abs
+-- this works but there is probs a cooler way
+
+split a = (map ((!!) a) [0,2..(length a - 1)],
+           map ((!!) a) [1,3..(length a - 1)])
+--cooler? ...idk
+
+
+-- d)
 --interleave :: ([a],[a]) -> [a]
 --
 ---- e)
