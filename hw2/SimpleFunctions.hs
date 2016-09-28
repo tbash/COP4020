1eadc56d49b106b45d5a2f8c539543f3e4b22ceb
T. Brandon Ashley <tb@tba.sh>
2016-09-28 10:12:57 -0400

    finish SimpleFunctions

--- hw2/SimpleFunctions.hs
+++ hw2/SimpleFunctions.hs
@@ -21,11 +21,11 @@ filterLast b a
 split :: [a] -> ([a],[a])
 split []        = ([], [])
 split [a]       = ([a], [])
---split (a:b:abs) = (a:as, b:bs)
---    where (as, bs) = split abs
+split (a:b:abs) = (a:as, b:bs)
+    where (as, bs) = split abs
 -- this works but there is probs a cooler way
 
-split a = (map ((!!) a)[0,2..(length a - 1)],map ((!!) a)[1,3..(length a - 1)])
+--split a = (map ((!!) a)[0,2..(length a - 1)],map ((!!) a)[1,3..(length a - 1)])
 --cooler? ...idk
 
 
@@ -33,7 +33,7 @@ split a = (map ((!!) a)[0,2..(length a - 1)],map ((!!) a)[1,3..(length a - 1)])
 interleave :: ([a],[a]) -> [a]
 interleave ([], a)  = a
 interleave (a, []) = a
-interleave (x:xs, y:ys) = x : y : interleave (xs, ys)
+interleave (a:as, b:bs) = a : b : interleave (as, bs)
 
 -- e)
 merge :: (Ord a) => ([a],[a]) -> [a]
@@ -43,7 +43,11 @@ merge (a:as, b:bs)
     | a < b     = a : merge (as, b:bs)
     | otherwise = b : merge (a:as, bs)
 
----- f)
---mergeSort :: (Ord a) => [a] -> [a]
-
+-- f)
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort [] = []
+mergeSort [a] = [a]
+mergeSort a  =
+    merge ((mergeSort as), (mergeSort bs))
+    where (as, bs) = split a
 
