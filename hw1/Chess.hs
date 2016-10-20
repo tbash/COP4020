56447971000bb605873709e50c8798b304e2dfd2
T. Brandon Ashley <tb@tba.sh>
2016-10-20 08:42:43 -0400

    :bowtie: rewrite chess for practice

--- hw1/Chess.hs
+++ hw1/Chess.hs
@@ -1,13 +1,12 @@
 module Chess where
 
-import Data.Char
-
-
 -- See https://en.wikipedia.org/wiki/Chess for more details
 -- We only consider the situation where there is only a single
 -- piece on the board
 
 -- see Rules - Set up for basic definitions
+
+import Data.Char
 
 type File     = Char         -- column index
                              -- valid files are 'a','b',...,'h'
@@ -25,73 +24,21 @@ data Piece =
 
 isLegalPosition :: Position -> Bool
 -- implement isLegalPosition
-isLegalPosition (a,b)
-     | isLegalRank b && isLegalFile a = True
-     | otherwise = False
-
-isLegalFile :: File -> Bool
-isLegalFile c
-     | c >= 'a' && c <= 'h' = True
-     | otherwise = False
-
-isLegalRank :: Rank -> Bool
-isLegalRank i
-     | i >= 1 && i <= 8 = True
-     | otherwise = False
+isLegalPosition (f, r) = f `elem` ['a'..'h'] && r `elem` [1..8]
 
 -- see Rules - Movement for legal movements
 
 isLegalMove :: Color -> Piece -> Position -> Position -> Bool
--- implement isLegalMove
-isLegalMove _ King (a,b) (c,d) =
-  if abs((ord a) - (ord c)) <= 1 && abs(b-d) <= 1
-    && isLegalPosition  (c,d)
-    then True
-    else False
-
-isLegalMove _ Queen (a,b) (c,d) =
-  if abs((ord a) - (ord c)) ==0 && abs(b - d)>=1 && isLegalPosition(c,d)
-    then True
-    else if abs((ord a)-(ord c)) >=1 && abs(b-d) ==0 && isLegalPosition(c,d)
-      then True
-      else if abs((ord a) - (ord c)) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)
-        then True
-        else False
-
-isLegalMove _ Bishop (a,b) (c,d) =
-  if abs((ord a)) - (ord c) == abs(b-d) && abs(b-d)/=0 && isLegalPosition(c,d)
-    then True
-    else False
-
-isLegalMove _ Rook (a,b) (c,d) =
-  if abs((ord a)-(ord c)) /=0 && abs(b-d) ==0 && isLegalPosition (c,d)
-    then True
-    else if abs((ord a)-(ord c)) ==0 && abs(b-d)/=0 && isLegalPosition(c,d)
-      then True
-      else False
-
-isLegalMove Black Pawn (a,b) (c,d) =
-  if abs((ord a)-(ord c))==0 && b - d == 1 && isLegalPosition(c,d)
-    then True
-    else if abs((ord a)-(ord c))==0 && b - d == 2 && b == 7 && isLegalPosition(c,d)
-      then True
-      else False
-
-isLegalMove White Pawn (a,b) (c,d) =
-  if abs((ord a)-(ord c))==0 && d - b == 1 && isLegalPosition(c,d)
-    then True
-    else if abs((ord a)-(ord c))==0 && d - b == 2 && b==2 && isLegalPosition(c,d)
-      then True
-      else False
-
-isLegalMove _ Knight (a,b) (c,d) =
-  if abs((ord a)-(ord c))==1 && abs(b-d)==2 && isLegalPosition(c,d)
-    then True
-    else if abs((ord a)-(ord c))==2 && abs(b-d) == 1 && isLegalPosition(c,d)
-      then True
-      else False
-
-isLegalMove _ _ _ _ = False
-
-doesStuff :: Char->Int
-doesStuff a = ord a
+isLegalMove c p (f1,r1) (f2,r2) = legalPs && case p of
+    King   -> diff `elem` [(0,0),(1,0),(0,1),(1,1)]
+    Queen  -> x == y || (x == 0 || y == 0)
+    Rook   -> (x == 0 || y == 0)
+    Bishop -> x == y
+    Knight -> diff `elem` [(0,0),(2,1),(1,2)]
+    Pawn   -> case c of
+                Black -> pDiff `elem` [(0,0),(0,2),(0,1)]
+                White -> pDiff `elem` [(0,0),(0,-2),(0,-1)]
+  where legalPs = isLegalPosition (f1,r1) && isLegalPosition (f2,r2)
+        diff    = (abs (ord f1 - ord f2), abs (r1 - r2))
+        pDiff   = (ord f1 - ord f2, r1 - r2)
+        (x,y)   = diff
